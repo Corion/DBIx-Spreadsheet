3e2a913a9e378459f01f5d014b4d67963e7a2bc0
Max Maischein <corion@corion.net>
2019-10-07 19:55:35 +0200

    Add documentation

--- lib/DBIx/Spreadsheet.pm
+++ lib/DBIx/Spreadsheet.pm
@@ -142,3 +142,37 @@ sub _import_data( $self ) {
 }
 
 1;
+
+=head1 SEE ALSO
+
+L<DBD::CSV>
+
+=head1 REPOSITORY
+
+The public repository of this module is
+L<http://github.com/Corion/DBIx-Spreadsheet>.
+
+=head1 SUPPORT
+
+The public support forum of this module is
+L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via the RT CPAN bug queue at
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=DBIx-Spreadsheet>
+or via mail to L<dbix-spreadsheet-Bugs@rt.cpan.org>.
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2019 by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the same terms as Perl itself.
+
+=cut
